c829bb87f9eec7cddb61b0ed684b1563559dea1a
Nanthakarn Limkool <nanthakarn18@gmail.com>
2021-10-16 17:48:35 +0700

    refactor and add UI
    - also add queue_report API [^^;]  555

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from string import Template
 from datetime import datetime
@@ -23,6 +23,10 @@ def parsing_date(birth_date: str):
 
 def delta_year(birth_date: datetime):
     return datetime.now().year - birth_date.year
+
+
+def is_citizen_ID(citizen_id):
+    return (citizen_id.isdigit() and len(citizen_id) == 13)
 
 
 class Citizen(db.Model):
@@ -77,22 +81,17 @@ class Reservation(db.Model):
     """
     __tablename__ = 'reservation'
     id = db.Column(db.Integer, primary_key=True)
-    citizen_id = db.Column(db.Numeric, unique=True)
+    citizen_id = db.Column(db.Numeric)
     site_name = db.Column(db.String(200))
     vaccine_name = db.Column(db.String(200))
-    timestamp = db.Column(db.Date)
-    queue = db.Column(db.Date)
-    checked = db.Column(db.Boolean)
+    timestamp = db.Column(db.DateTime, default=datetime.now())
+    queue = db.Column(db.DateTime, default=None)
+    checked = db.Column(db.Boolean, default=False)
 
-    # vaccine_taken = db.Column(db.PickleType(mutable=True))
-
-    def __init__(self, citizen_id, site_name, vaccine_name, timestamp, queue, checked):
+    def __init__(self, citizen_id, site_name, vaccine_name):
         self.citizen_id = citizen_id
         self.site_name = site_name
         self.vaccine_name = vaccine_name
-        self.timestamp = timestamp
-        self.queue = queue
-        self.checked = checked
 
     def __str__(self):
         return f"""
@@ -100,8 +99,8 @@ class Reservation(db.Model):
             <th scope="row">{self.citizen_id}</th>
             <td>{self.site_name}</td>
             <td>{self.vaccine_name}</td>
-            <td>{self.timestamp}</td>
-            <td>{self.queue}</td>
+            <td>{self.timestamp.strftime("%Y-%m-%d, %H:%M:%S")}</td>
+            <td>{self.queue.strftime("%Y-%m-%d, %H:%M:%S") if self.queue else "TBC"}</td>
             <td>{self.checked}</td>
         </tr>
         """
@@ -119,35 +118,40 @@ def registration_usage():
 
 @app.route('/registration', methods=['POST'])
 def registration():
-    if request.method == 'POST':
-        citizen_id = request.values['citizen_id']
-        name = request.values['name']
-        surname = request.values['surname']
-        birth_date = request.values['birth_date']
-        occupation = request.values['occupation']
-        address = request.values['address']
+    citizen_id = request.values['citizen_id']
+    name = request.values['name']
+    surname = request.values['surname']
+    birth_date = request.values['birth_date']
+    occupation = request.values['occupation']
+    address = request.values['address']
+    
+    if not (citizen_id and name and surname and birth_date and occupation
+            and address):
+        return {"feedback": "registration fail: missing some attribute"}
 
-        if not (citizen_id and name and surname and birth_date and occupation
-                and address):
-            return {"feedback": "registration fail: missing some attribute"}
+    if not is_citizen_ID(citizen_id):
+        return {"feedback": "registration fail: invalid citizen ID"}
+    
+    try:
+        birth_date = parsing_date(birth_date)
+        if delta_year(birth_date) <= 12:
+            return {"feedback": "registration fail: not archived minimum age"}
+    except ValueError:
+        return {"feedback": "registration fail: invalid birth date format"}
 
-        if not (citizen_id.isdigit() and len(citizen_id) == 13):
-            return {"feedback": "registration fail: invalid citizen ID"}
+    if db.session.query(Citizen).filter(
+            Citizen.citizen_id == citizen_id).count() > 0:
+        return {"feedback": "registration fail: this person already registed"}
 
-        try:
-            birth_date = parsing_date(birth_date)
-            if delta_year(birth_date) <= 12:
-                return {"feedback": "registration fail: not archived minimum age"}
-        except ValueError:
-            return {"feedback": "registration fail: invalid birth date format"}
+    data = Citizen(int(citizen_id), name, surname, birth_date, occupation, address)
+    db.session.add(data)
+    db.session.commit()
+    return {"feedback": "registration success!"}
 
-        if db.session.query(Citizen).filter(Citizen.citizen_id == citizen_id).count() > 0:
-            return {"feedback": "registration fail: this person already registed"}
 
-        data = Citizen(int(citizen_id), name, surname, birth_date, occupation, address)
-        db.session.add(data)
-        db.session.commit()
-        return {"feedback": "registration success!"}
+@app.route('/reservation', methods=['GET'])
+def reservation_usage():
+    return render_template('reservation.html')
 
 
 @app.route('/reservation', methods=['POST'])
@@ -155,60 +159,93 @@ def reservation():
     """
     Add reservation data to the database
     """
-    if request.method == 'POST':
-        citizen_id = request.values['citizen_id']
-        site_name = request.values['site_name']
-        vaccine_name = request.values['vaccine_name']
-        timestamp = datetime.now()
-        queue = request.values['queue']
-        checked = request.values.get('checked', type=bool)
-        json_data = {"citizen_id": citizen_id, "site_name": site_name, "vaccine_name": vaccine_name, "timestamp": timestamp, "queue": queue, "checked": checked}
+    citizen_id = request.values['citizen_id']
+    site_name = request.values['site_name']
+    vaccine_name = request.values['vaccine_name']
 
-        if not (citizen_id and site_name and vaccine_name and timestamp):
-            return {"feedback": "reservation fail: missing some attribute"}
+    if not (citizen_id and site_name and vaccine_name):
+        return {"feedback": "reservation fail: missing some attribute"}
 
-        if not (citizen_id.isdigit() and len(citizen_id) == 13): 
-            return {"feedback": "reservation fail: invalid citizen ID"}
+    if not is_citizen_ID(citizen_id):
+        return {"feedback": "reservation fail: invalid citizen ID"}
 
-        if db.session.query(Citizen).filter(Citizen.citizen_id == citizen_id).count() <= 0:
-            return {"feedback": "reservation fail: citizen ID is not registered"}
+    if db.session.query(Citizen).filter(
+            Citizen.citizen_id == citizen_id).count() <= 0:
+        return {"feedback": "reservation fail: citizen ID is not registered"}
 
-        try:
-            queue = parsing_date(queue)  # Convert queue to datetime object.
-            if queue <= datetime.now():  # Can only reserve vaccine in the future.
-                return jsonify(json_data)
-        except ValueError:  # If queue can't be convert to datetime object.
-            return jsonify(json_data)
-
-        data = Reservation(int(citizen_id), site_name, vaccine_name, timestamp, queue, checked)
-        db.session.add(data)
-        db.session.commit()
-        return {"feedback": "reservation success!"}
+    data = Reservation(int(citizen_id), site_name, vaccine_name)
+    db.session.add(data)
+    db.session.commit()
+    return {"feedback": "reservation success!"}
 
 
-@app.route('/cancel_reservation', methods=['DELETE'])
+@app.route('/queue_report', methods=['POST'])
+def update_queue():
+    citizen_id = request.values['citizen_id']
+    queue = request.values['queue']
+    try:
+        queue = datetime.strptime(queue, "%Y-%m-%d %H:%M:%S.%f")
+        if queue <= datetime.now():
+            return {
+                "feedback":
+                "report fail: can only reserve vaccine in the future"
+            }
+    except ValueError:
+        return {"feedback": "report fail: invalid queue datetime format"}
+
+    reservation = db.session.query(Reservation).filter(Reservation.citizen_id == citizen_id).first()
+    reservation.queue = queue
+    db.session.commit()
+    return {"feedback": "report success!"}
+
+
+@app.route('/reservation', methods=['DELETE'])
 def cancel_reservation():
     """
     Cancel reservation and remove it from the database.
     """
-    if request.method == 'DELETE':
-        citizen_id = request.values['citizen_id']
+    citizen_id = request.values['citizen_id']
+    if not (citizen_id):
+        return {"feedback": "cancel reservation failed: no citizen id is given"}
 
-        if not (citizen_id):
-            return {"feedback": "cancel reservation failed: no citizen id is given"}
+    if not is_citizen_ID(citizen_id):
+        return {"feedback": "cancel reservation failed: invalid citizen ID"}
 
-        if not (citizen_id.isdigit() and len(citizen_id) == 13): 
-            return {"feedback": "cancel reservation failed: invalid citizen ID"}
+    data = db.session.query(Reservation).filter(Reservation.citizen_id == citizen_id)
+    if data.count() == 0:
+        return {
+            "feedback":
+            "cancel reservation failed: there is no reservation for this citizen id"
+        }
 
-        data = db.session.query(Reservation).filter(Reservation.citizen_id == citizen_id)
+    for elem in data:
+        db.session.delete(elem)
 
-        if data.count() == 0:
-            return {"feedback": "cancel reservation failed: there is no reservation for this citizen id"}
+    db.session.commit()
+    return {"feedback": "cancel reservation successfully"}
 
-        for elem in data:
-            db.session.delete(elem)
-        db.session.commit()
-    return {"feedback" : "cancel reservation successfully"}
+
+@app.route('/reservation_database', methods=['GET'])
+def reservation_database():
+    tbody = ""
+    for reservation in db.session.query(Reservation).all():
+        tbody += str(reservation)
+
+    html = render_template('database.html')
+    html = Template(html).safe_substitute(
+        title="Reservation",
+        count=db.session.query(Reservation).count(),
+        unit="reservation(s)",
+        thead="""<tr>
+            <th scope="col">Citizen ID</th>
+            <th scope="col">Site Name</th>
+            <th scope="col">Vaccine Name</th>
+            <th scope="col">Timestamp</th>
+            <th scope="col">Queue</th>
+            <th scope="col">Checked</th>
+            </tr>""",
+        tbody=tbody)
+    return html
 
 
 @app.route('/citizen', methods=['GET'])
@@ -222,7 +259,6 @@ def citizen():
         title="Citizen",
         count=db.session.query(Citizen).count(),
         unit="person(s)",
-        note="",
         thead="""<tr>
             <th scope="col">Citizen ID</th>
             <th scope="col">Firstname</th>
